99b73742d24676de91a96f5ec16b19dee0a668f3
Nuhaila <nuasai04@gmail.com>
2025-10-08 16:42:27 +0200

    creacion de ejercicio

---
+++ src/test/java/es/etg/daw/dawes/java/rest/restful/RestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package es.etg.daw.dawes.java.rest.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
