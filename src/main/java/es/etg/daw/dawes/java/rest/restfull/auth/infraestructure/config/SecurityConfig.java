4471f7da2bb286ae68ac6b46b31e53ff12dd10fe
Nuhaila <nuhailaassaid16@gmail.com>
2026-01-14 10:44:58 -0600

    agregamos un bean en la configuracion

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.db.jpa.repository.UserEntityRepository;
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.mapper.UserMapper;
@@ -33,5 +36,16 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    
+        http.authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/categorias/**").hasRole("ADMIN")
+            .requestMatchers("/productos/**").hasAnyRole("USER", "ADMIN"));
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
     }
 }
