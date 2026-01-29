d257f6d9c8b9607232f2ac45e8e5df16af340af2
Nuhaila <nuhailaassaid16@gmail.com>
2026-01-29 11:40:42 -0600

    agrego la parte de perfiles an application.properties

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +13,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.db.jpa.repository.UserEntityRepository;
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.mapper.UserMapper;
@@ -38,6 +44,7 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
+    /* 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     
@@ -47,5 +54,41 @@ public class SecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    */
+
+    
+    //para probar la parte de pinta y colorea
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .cors(cors -> cors.configure(http)) // Habilita CORS dentro de Spring Security
+        .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para pruebas (opcional)
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    
+        return http.build();
+    }
+
+    /** Configuración para evitar el error de CORS que dará el navegador */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        // Permitimos todos los orígenes
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        
+        // Permitimos los métodos necesarios
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        // Permitimos todas las cabeceras
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        
+        // Permitimos que el navegador reciba la respuesta
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
