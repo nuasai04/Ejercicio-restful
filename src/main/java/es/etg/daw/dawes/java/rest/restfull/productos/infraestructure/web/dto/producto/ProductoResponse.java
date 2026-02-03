16d19c34c1709b702286444382e962a70cef8b2c
Nuhaila <nuhailaassaid16@gmail.com>
2026-02-03 13:03:13 -0600

    agregar información tambien en los DTOs/Modelos de nuestros servicios, para ello usamos la anotacion schema en productoResponse

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/dto/producto/ProductoResponse.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/dto/producto/ProductoResponse.java
@@ -6,6 +6,23 @@ package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.dto.pr
 
 import java.time.LocalDateTime;
 
-public record ProductoResponse(int id, String nombre, double precio, LocalDateTime createdAt, int categoria) {
-	
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ProductoResponse(
+    
+    @Schema(description = "ID único del producto", example = "23")
+    int id, 
+    
+    @Schema(description = "Nombre comercial del producto", example = "Monitor Gamer")
+    String nombre, 
+    
+    @Schema(description = "Precio unitario en euros", example = "23")
+    double precio, 
+    
+    @Schema(description = "Fecha y hora de creación del registro", example = "2024-05-20T14:30:00")
+    LocalDateTime createdAt, 
+    
+    @Schema(description = "ID de la categoría asociada", example = "1")
+    int categoria
+) {
 }
